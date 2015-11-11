124562a13804e0a620985b0df910cfb3ef5af015
John Stechschulte <john.l.stechschulte@gmail.com>
2015-11-11 13:16:00 -0700

    wrote setup.m to load data and generate training and testing sets

---
+++ matlab/setup.m
@@ -0,0 +1,12 @@
+% Load data
+load('../data/nyu_depth_data_labeled.mat');
+%load('../data/nyu_depth_v2_labeled.mat');
+
+% Training/testing split
+rng(42, 'twister');
+
+N = size(images, 4);
+test_size = floor(N/10);
+
+testing = randsample(N, test_size);
+training = setdiff(1:N, testing);
