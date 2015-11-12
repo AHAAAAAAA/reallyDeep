c801fe9ba412ce0755197f02af03838995e4ab8a
John Stechschulte <john.l.stechschulte@gmail.com>
2015-11-12 13:52:43 -0700

    more baseline

--- matlab/baseline.m
+++ matlab/baseline.m
@@ -9,8 +9,17 @@ for c = 1:C
   for i=1:I
     avg_depths(i) = extract_object_depth(dpts_trn(:,:,i), lbls_trn(:,:,i), 1);
   end
-  % TODO:
-  % reshape inputs into array of size (640*480*3 + 640*480 + 640*480) x I
-  % run mvregress on input, ^, and avg_depths as output
+  inputs = [reshape(imgs_trn, 640*480*3, []); reshape(lbls_trn, 640*480, [])]; % 640*480*4 x I
+  [beta, Sigma] = mvregress(inputs', avg_depths);
+
   % test regression on _tst datasets
+  [imgs_tst, dpts_tst, lbls_tst] = get_dataset(images_tst, depths_tst, labels_tst, c);
+  I = length(imgs_tst(1,1,1,:));
+  avg_depths = zeros(I, 1);
+  for i=1:I
+    avg_depths(i) = extract_object_depth(dpts_tst(:,:,i), lbls_tst(:,:,i), 1);
+  end
+  inputs = [reshape(imgs_tst, 640*480*3, []); reshape(lbls_tst, 640*480, [])]; % 640*480*4 x I
+  err = beta'*inputs - avg_depths;
+  break;
 end
