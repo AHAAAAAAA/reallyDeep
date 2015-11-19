5165f054436ad1e31695e21c3822cec9dd6015aa
wuqingjun <wuqingjun@gmail.com>
2015-11-19 00:08:04 -0700

    improved

--- matlab/analyse_pca.m
+++ matlab/analyse_pca.m
@@ -1,20 +1,29 @@
-[x,y,z] = size(depths_trn)
-R =[] % zeros(z, x * y);
+load ../data/dev_dataset.mat
+
+granu=20;
+
+[X, Y, Z] = size(depths_trn);
+[M, N, L] = size(depths_tst);
+
 
 class_type = 'sofa';
 class_ind = namesToIds(class_type);
 
 % get indices of images in training and test sets
 trn_inds = find_rel_pics(labels_trn,class_ind);
-length(trn_inds);
 tst_inds = find_rel_pics(labels_tst,class_ind);
-length(tst_inds);
+tst_not_inds = setdiff((1:L), tst_inds)
 
-for j=1:length(trn_inds)
-    i=trn_inds(j);
-    A = depths_trn(:, :, i);
-    A = reshape(A, 1, x * y);
-    R = [R;A];
-end
+R = generate_feature_matrix(trn_inds, depths_trn, granu, X, Y);
 
-[COEFF, SCORE, LATENT] = princomp(R);
+[W, pc] = princomp(R);
+R = generate_feature_matrix((1:L), depths_tst, granu, X, Y)
+
+P=R * W;
+
+P=P(:, 1:3);
+
+save('g20sofaDev.mat', 'granu', 'class_type', 'class_ind', 'trn_inds', 'tst_inds', 'W', 'pc', 'P')
+
+%plot(pc(1,:),pc(2,:),'.'); 
+%title('{\bf PCA} by princomp'); xlabel('PC 1'); ylabel('PC 2')
