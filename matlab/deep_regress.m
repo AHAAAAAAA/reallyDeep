581936ed25326ed572e9cb002ec14efd6d6c95f7
John Stechschulte <john.l.stechschulte@gmail.com>
2015-12-06 15:21:15 -0700

    basic deep_regress function working

--- matlab/deep_regress.m
+++ matlab/deep_regress.m
@@ -1,11 +1,7 @@
 % train regression for given object class and evaluate on test datasets
-% provided
+% provided; the labels contain just 0/1 to only indicate the object of interest
 
-function [] = deep_regress(class, images_trn, depths_trn, labels_trn, images_tst, depths_tst, labels_tst)
-
-% extract data subsets
-[imgs_trn, dpts_trn, lbls_trn] = get_dataset(images_trn, depths_trn, labels_trn, class);
-[imgs_tst, dpts_tst, lbls_tst] = get_dataset(images_tst, depths_tst, labels_tst, class);
+function [avg_depth_trn, y_trn_pred, avg_depth_tst, y_tst_pred] = deep_regress(images_trn, depths_trn, labels_trn, images_tst, depths_tst, labels_tst)
 
 % parameters--TODO: convert (some? all?) these to function parameters with default values
 er_r = 10; % Image erode filter radius
@@ -13,84 +9,120 @@ di_r = 8; % Image dialation filter radius
 n_pix_thresh = 1000; % minimum number of pixels to accept for training
 dist_thresh = 3.5; % maximum distance to accept for training
 c_points = 10; % number of corners to detect
-do_obj_separation = true;
 do_corners = false;
 
 im_x_mid = 640/2;
 im_y_mid = 480/2;
 
 % Feature Training Extraction
-npix_trn = [];
-dx_trn = [];
-dy_trn = [];
-x_trn = [];
-y_trn = [];
-cn_trn = [];
-cx_trn = [];
-cy_trn = [];
+npix_trn = []; npix_tst = [];
+dx_trn = [];   dx_tst = [];
+dy_trn = [];   dy_tst = [];
+x_trn = [];    x_tst = [];
+y_trn = [];    y_tst = [];
+cn_trn = [];   cn_tst = [];
+cx_trn = [];   cx_tst = [];
+cy_trn = [];   cy_tst = [];
 
 % Label Training Extraction
-avg_depth_trn = [];
+avg_depth_trn = []; avg_depth_tst = [];
 
-% build feature vectors
+% build training feature vectors
 loop_count_trn = 0;
-for ii=1:length(imgs_trn)
-  matrix_z = lbls_trn(:,:,ii)==class_ind;
-  image_gray = rgb2gray(imgs_trn(:,:,:,ii));
+for ii=1:size(images_trn, 4)
+  image_gray = rgb2gray(images_trn(:,:,:,ii));
 
-  if do_obj_separation
-    % Erode and dilate to isolate separate object instances
-    se_er = strel('disk',er_r);
-    se_di = strel('disk',di_r);
+  [obj_inds,obj_x_inds,obj_y_inds,n_pix,obj_dx,obj_dy,obj_pres] = extract_obj(labels_trn(:,:,ii),1);
+  avg_depth = extract_object_depth(depths_trn(:,:,ii),labels_trn(:,:,ii),1);
 
-    mat_z_er = imerode(matrix_z,se_er);
-    mat_z_di = imdilate(mat_z_er,se_di);
+  if((avg_depth<dist_thresh) && (n_pix>n_pix_thresh))
+    loop_count_trn = loop_count_trn+1;
+    obj_x_mid = mean(obj_x_inds);
+    obj_y_mid = mean(obj_y_inds);
+    % Append!
+    npix_trn = [npix_trn; n_pix];
+    dx_trn = [dx_trn; obj_dx];
+    dy_trn = [dy_trn; obj_dy];
+    x_trn = [x_trn; obj_x_mid];
+    y_trn = [y_trn; obj_y_mid];
+    avg_depth_trn = [avg_depth_trn; avg_depth];
 
-    CC = bwconncomp(mat_z_di);
-  else
-    CC = struct('NumObjects', 1, 'PixelIdxList', {find(matrix_z)});
-  end
+    if do_corners
+      isolated_image = image_gray .* labels(:,:,ii);
 
-  for kk = 1:CC.NumObjects
-    ind_kk = CC.PixelIdxList{kk};
-    matrix_ziso = zeros(480,640);
-    matrix_ziso(ind_kk) = 1;
-
-    [obj_inds,obj_x_inds,obj_y_inds,n_pix,obj_dx,obj_dy,obj_pres] = extract_obj(matrix_ziso,1);
-    avg_depth = extract_object_depth(dpts_trn(:,:,ii),matrix_ziso,1);
-
-    if((avg_depth<dist_thresh) && (n_pix>n_pix_thresh))
-      loop_count_trn = loop_count_trn+1;
-      obj_x_mid = mean(obj_x_inds);
-      obj_y_mid = mean(obj_y_inds);
-      % Append!
-      npix_trn = [npix_trn; n_pix];
-      dx_trn = [dx_trn; obj_dx];
-      dy_trn = [dy_trn; obj_dy];
-      x_trn = [x_trn; obj_x_mid];
-      y_trn = [y_trn; obj_y_mid];
-      avg_depth_trn = [avg_depth_trn; avg_depth];
-
-      if do_corners
-        isolated_image = image_gray .* matrix_ziso;
-
-        % find corners
-        C = corner(mat_shift);
-        Cn = size(C,1);
-        if Cn < c_points
-          C = [C; zeros(c_points - Cn, 2)];
-        else
-          C = C(1:c_points,:);
-        end
-
-        % Append!
-        cn_trn = [cn_trn; Cn];
-        cx_trn = [cx_trn; (C(:,1)).'];
-        cy_trn = [cy_trn; (C(:,2)).'];
+      % find corners
+      C = corner(mat_shift);
+      Cn = size(C,1);
+      if Cn < c_points
+        C = [C; zeros(c_points - Cn, 2)];
+      else
+        C = C(1:c_points,:);
       end
+
+      % Append!
+      cn_trn = [cn_trn; Cn];
+      cx_trn = [cx_trn; (C(:,1)).'];
+      cy_trn = [cy_trn; (C(:,2)).'];
     end
   end
 end
 
+% build testing feature vectors
+loop_count_tst = 0;
+for ii=1:size(images_tst, 4)
+  image_gray = rgb2gray(images_tst(:,:,:,ii));
+
+  [obj_inds,obj_x_inds,obj_y_inds,n_pix,obj_dx,obj_dy,obj_pres] = extract_obj(labels_tst(:,:,ii),1);
+  avg_depth = extract_object_depth(depths_tst(:,:,ii),labels_tst(:,:,ii),1);
+
+  if((avg_depth<dist_thresh) && (n_pix>n_pix_thresh))
+    loop_count_tst = loop_count_tst+1;
+    obj_x_mid = mean(obj_x_inds);
+    obj_y_mid = mean(obj_y_inds);
+    % Append!
+    npix_tst = [npix_tst; n_pix];
+    dx_tst = [dx_tst; obj_dx];
+    dy_tst = [dy_tst; obj_dy];
+    x_tst = [x_tst; obj_x_mid];
+    y_tst = [y_tst; obj_y_mid];
+    avg_depth_tst = [avg_depth_tst; avg_depth];
+
+    if do_corners
+      isolated_image = image_gray .* labels(:,:,ii);
+
+      % find corners
+      C = corner(mat_shift);
+      Cn = size(C,1);
+      if Cn < c_points
+        C = [C; zeros(c_points - Cn, 2)];
+      else
+        C = C(1:c_points,:);
+      end
+
+      % Append!
+      cn_tst = [cn_tst; Cn];
+      cx_tst = [cx_tst; (C(:,1)).'];
+      cy_tst = [cy_tst; (C(:,2)).'];
+    end
+  end
+end
+
+x_in = [npix_trn, dx_trn, dy_trn, x_trn, y_trn, atan(dx_trn/2), tan(dy_trn/2), ones(size(npix_trn))]; % final is bias term
+x_in_tst = [npix_tst, dx_tst, dy_tst, x_tst, y_tst, atan(dx_tst/2), tan(dy_tst/2), ones(size(npix_tst))]; % final is bias term
+if do_corners
+  x_in = [x_in, cn_trn, cx_trn, cy_trn];
+  x_in_tst = [x_in, cn_tst, cx_tst, cy_tst];
+end
+
+y_lab = avg_depth_trn;
+
+size(x_in)
+size(y_lab)
+[beta, sigma] = mvregress(x_in, y_lab);
+y_trn_pred = x_in * beta;
+
+y_tst_pred = x_in_tst * beta;
+
+% TODO: also calculate baseline(s)
 
 end
