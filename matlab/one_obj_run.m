0f9a3f337d8b9aa6f18dcacf91ca94972e89e701
wuqingjun <wuqingjun@gmail.com>
2015-12-13 22:59:37 -0700

    save file to tmp

--- matlab/one_obj_run.m
+++ matlab/one_obj_run.m
@@ -103,8 +103,8 @@ boldify
 %% Feature Analysis
 [dum_val,f_best] = min(beta_stat);
 [dum_val,f_worst] = max(beta_stat);
-% f_best = 2;
-save('oneobjrunresult.m', 'avg_depth_trn', 'trn_sort', 'f_best', 'f_worst', 'feat_str', 'beta_stat', 'class_type', 'avg_depth_tst', 'x_in_tst', 'x_in'); 
+f_best = 10;
+save('/tmp/oneobjrunresult.mat', 'avg_depth_trn', 'trn_sort', 'f_best', 'f_worst', 'feat_str', 'beta_stat', 'class_type', 'avg_depth_tst', 'x_in_tst', 'x_in', 'tst_sort'); 
 figure(3)
 clf(3)
 subplot(2,3,1)
